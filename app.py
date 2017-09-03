5201c98565ab7bf4db2b5adf41422456013254fc
SamDerp <samuelderpyson@gmail.com>
2017-09-03 20:15:23 +0100

    Version 1.4
    Refactored code, using globals TEMPORARILY to communicate between functions

--- app.py
+++ app.py
@@ -1,50 +1,69 @@
 from random import *
+from operator import *
 
+randomNumber1 = 0
+randomNumber2 = 0
+randomSign = 0
+studentScore = 0
+studentName = 0
+questionNumber = 0
 
 def startup():
-    print("This is a basic Maths quiz, there will be 10 questions, answer each question, at the end you will get a score out of 10.\n\n")
+    print("This is a basic Maths quiz, answer each question, at the end you will get a score.\n\n")
     while True:
-        quizOrQuit = input("Do you wish to participate? (Y/N)")
-        if quizOrQuit.lower() not in ("yesno"):
-            print("Not an appropriate choice.")
+        quizOrQuit = input("Do you wish to participate? (Y/N)").lower()
+        if quizOrQuit not in ("yesno"):
+            print("Not a valid input.")
         else:
             break
-    if quizOrQuit.lower() in "yes":
-        mathsQuiz()
+    if quizOrQuit in "yes":
+        startMathsQuiz()
     else:
         quit()
 
 
-def mathsQuiz():
+def startMathsQuiz():
+    questionNumber = 0
     studentName = str(input("What is your name? "))
-    studentScore = 0
-    questionNumber = 1
-    while questionNumber != 11:
-        print("\n\nQuestion No." + str(questionNumber))
-        randomNumber1 = randint(0,10)
-        randomNumber2 = randint(0,10)
-        randomSign = choice(["+", "-", "*"])
-        print("What is " + str(randomNumber1) + " " + randomSign + " " + str(randomNumber2)+"?")
-        while True:
-            try:
-                questionGuess = int(input("Enter your answer: "))
-                break
-            except:
-                continue
-        if randomSign == "+":
-            questionAnswer = randomNumber1 + randomNumber2
-        elif randomSign == "-":
-            questionAnswer = randomNumber1 - randomNumber2
-        else:
-            questionAnswer = randomNumber1 * randomNumber2
-        if questionGuess == questionAnswer:
-            print("Correct. ")
-            studentScore += 1
-        else:
-            print("Incorrect. ")
-        print("You are currently on " + str(studentScore) + "/" + str(questionNumber))
-        questionNumber += 1
-    print(studentName + ", Your final score was " + str(studentScore) + " out of 10.")
+    while True:
+        try:
+            maxQuestions = int(input("Enter the number of questions in integer form. "))
+            break
+        except:
+            print("That is an invalid input. ")
+            continue
+    for questionNumber in range(1,maxQuestions + 1):
+        askQuestion()
+        print("\nYou are currently on {} / {}".format(studentScore, questionNumber))
+    print("{} , your final score was {} / {}".format(studentName, studentScore, questionNumber))
 
+
+def randomGenerator():
+    global randomNumber1
+    global randomNumber2
+    global randomSign
+    randomNumber1 = randint(0,10)
+    randomNumber2 = randint(0,10)
+    randomSign = choice(["+", "-", "*"])
+    
+
+def askQuestion():
+    randomGenerator()
+    global studentScore
+    operator = {"+": add,
+                "-": sub,
+                "*": mul}
+    while True:
+        try:
+            studentGuess = int(input("Enter the answer to {} {} {}\n".format(randomNumber1, randomSign, randomNumber2)))
+            break
+        except:
+            print("That is an invalid answer, try again.")
+            continue
+    if studentGuess == operator[randomSign](randomNumber1, randomNumber2):
+        print("Correct.")
+        studentScore += 1
+    else:
+        print("Incorrect.")
 
 startup()
