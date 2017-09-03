02859b9c8fea8f0f289726956a2108ad91b40e07
SamDerp <samuelderpyson@gmail.com>
2017-09-03 20:10:01 +0100

    Version 1.2
    Contained quiz within function, added startup function with option to quit

--- app.py
+++ app.py
@@ -1,46 +1,41 @@
 from random import * #This method is prefered over 'import random' as it means 'random' prefix isn't required
-print("This is a basic Maths quiz, there will be 10 questions, answer each question, at the end you will get a score out of 10.")
-studentName = str(input("What's your name? "))
-studentScore = 0
-questionNumber = 1
-while questionNumber != 11:
-    print("\n\nQuestion No." + str(questionNumber))
-    randomNumber1 = randint(0,10)
-    randomNumber2 = randint(0,10)
-    randomSign = choice(["+", "-", "*"])
-    print("What is " + str(randomNumber1) + " " + randomSign + " " + str(randomNumber2)+"?")#Changing randomNumber2 to a string, to make output have a questionmark at the end of the number, not a space inbetween 
-    questionGuess = int(input("Enter your answer: "))
-    if randomSign == "+":
-        questionAnswer = randomNumber1 + randomNumber2
-    elif randomSign == "-":
-        questionAnswer = randomNumber1 - randomNumber2
+def startup():
+    print("This is a basic Maths quiz, there will be 10 questions, answer each question, at the end you will get a score out of 10.\n\n")
+    quizOrQuit = input("Do you wish to participate? (Y/N)")
+    if quizOrQuit == "Y":
+        mathsQuiz()
     else:
-        questionAnswer = randomNumber1 * randomNumber2
-    if questionGuess == questionAnswer:
-        print("Correct. ")
-        studentScore += 1
-    else:
-        print("Incorrect. ")
-    print("You are currently on " + str(studentScore) + "/" + str(questionNumber))
-    questionNumber += 1
-print(studentName + ", Your final score was " + str(studentScore) + " out of 10.")
-
-
-
-
-
-
-
-
-
-
-
-'''
-Version 1.1 Goals
-Get subtraction and multiplication working, have it choose randomly between multiplication, addition, and subtraction.
-Version 1.0 Goals
-Get basic maths addition quiz working;
-10 random addition questions, and return a final score out of 10.
-Ask for students name
-'''
-
+        quit()
+    
+    
+def mathsQuiz():
+    studentName = input("What is your name? ")
+    studentScore = 0
+    questionNumber = 1
+    while questionNumber != 11:
+        print("\n\nQuestion No." + str(questionNumber))
+        randomNumber1 = randint(0,10)
+        randomNumber2 = randint(0,10)
+        randomSign = choice(["+", "-", "*"])
+        print("What is " + str(randomNumber1) + " " + randomSign + " " + str(randomNumber2)+"?")#Changing randomNumber2 to a string, to make output have a questionmark at the end of the number, not a space inbetween 
+        while True:
+            try:
+                questionGuess = int(input("Enter your answer: "))
+                break
+            except:
+                continue
+        if randomSign == "+":
+            questionAnswer = randomNumber1 + randomNumber2
+        elif randomSign == "-":
+            questionAnswer = randomNumber1 - randomNumber2
+        else:
+            questionAnswer = randomNumber1 * randomNumber2
+        if questionGuess == questionAnswer:
+            print("Correct. ")
+            studentScore += 1
+        else:
+            print("Incorrect. ")
+        print("You are currently on " + str(studentScore) + "/" + str(questionNumber))
+        questionNumber += 1
+    print(studentName + ", Your final score was " + str(studentScore) + " out of 10.")
+startup()
