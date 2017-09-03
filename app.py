23551da862417eb5e04c059d305e41bf413c3618
SamDerp <samuelderpyson@gmail.com>
2017-09-03 20:17:17 +0100

    Version 1.5
    Housed everything within a class, to get rid of globals

--- app.py
+++ app.py
@@ -1,69 +1,65 @@
 from random import *
 from operator import *
 
-randomNumber1 = 0
-randomNumber2 = 0
-randomSign = 0
-studentScore = 0
-studentName = 0
-questionNumber = 0
+class MathsQuiz(object):
 
-def startup():
-    print("This is a basic Maths quiz, answer each question, at the end you will get a score.\n\n")
-    while True:
-        quizOrQuit = input("Do you wish to participate? (Y/N)").lower()
-        if quizOrQuit not in ("yesno"):
-            print("Not a valid input.")
+    def __init__(self):
+        self.randomNumber1 = 0
+        self.randomNumber2 = 0
+        self.randomSign = 0
+        self.studentScore = 0
+        self.studentName = None
+
+    def startup(self):
+        print("This is a basic Maths quiz, answer each question, at the end you will get a score.\n\n")
+        while True:
+            quizOrQuit = input("Do you wish to participate? (Y/N)").lower()
+            if quizOrQuit not in ("yesno"):
+                print("Not a valid input.")
+            else:
+                break
+        if quizOrQuit in "yes":
+            self.startQuiz()
         else:
-            break
-    if quizOrQuit in "yes":
-        startMathsQuiz()
-    else:
-        quit()
+            quit()
 
+    def startQuiz(self):
+        questionNumber = 0
+        studentName = str(input("What is your name? "))
+        while True:
+            try:
+                maxQuestions = int(input("Enter the number of questions in integer form. "))
+                break
+            except:
+                print("That is an invalid input. ")
+                continue
+        for questionNumber in range(1,maxQuestions + 1):
+            self.askQuestion()
+            print("\nYou are currently on {} / {}".format(self.studentScore, questionNumber))
+        print("{} , your final score was {} / {}".format(studentName, self.studentScore, questionNumber))
 
-def startMathsQuiz():
-    questionNumber = 0
-    studentName = str(input("What is your name? "))
-    while True:
-        try:
-            maxQuestions = int(input("Enter the number of questions in integer form. "))
-            break
-        except:
-            print("That is an invalid input. ")
-            continue
-    for questionNumber in range(1,maxQuestions + 1):
-        askQuestion()
-        print("\nYou are currently on {} / {}".format(studentScore, questionNumber))
-    print("{} , your final score was {} / {}".format(studentName, studentScore, questionNumber))
+    def randomGenerator(self):
+        self.randomNumber1 = randint(0,10)
+        self.randomNumber2 = randint(0,10)
+        self.randomSign = choice(["+", "-", "*"])
 
+    def askQuestion(self):
+        self.randomGenerator()
+        operator = {"+": add,
+                    "-": sub,
+                    "*": mul}
+        while True:
+            try:
+                studentGuess = int(input("Enter the answer to {} {} {}\n".format(self.randomNumber1, self.randomSign, self.randomNumber2)))
+                break
+            except:
+                print("That is an invalid answer, try again.")
+                continue
+        if studentGuess == operator[self.randomSign](self.randomNumber1, self.randomNumber2):
+            print("Correct.")
+            self.studentScore += 1
+        else:
+            print("Incorrect.")
 
-def randomGenerator():
-    global randomNumber1
-    global randomNumber2
-    global randomSign
-    randomNumber1 = randint(0,10)
-    randomNumber2 = randint(0,10)
-    randomSign = choice(["+", "-", "*"])
-    
-
-def askQuestion():
-    randomGenerator()
-    global studentScore
-    operator = {"+": add,
-                "-": sub,
-                "*": mul}
-    while True:
-        try:
-            studentGuess = int(input("Enter the answer to {} {} {}\n".format(randomNumber1, randomSign, randomNumber2)))
-            break
-        except:
-            print("That is an invalid answer, try again.")
-            continue
-    if studentGuess == operator[randomSign](randomNumber1, randomNumber2):
-        print("Correct.")
-        studentScore += 1
-    else:
-        print("Incorrect.")
-
-startup()
+student = MathsQuiz()
+student.startup()
