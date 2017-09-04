8795a9df91a2c9d146b265c0a2281e91ba78ea05
SamDerp <samuelderpyson@gmail.com>
2017-09-04 02:54:51 +0100

    Version 1.7.1
    Further formatting to follow PEP 8 & stop making IDEA angry at me.

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ student_users = {}
 
 
 class MathsQuiz(object):
-
     def __init__(self, studentName):
         self.studentName = studentName
         self.randomNumber1 = 0
@@ -16,16 +15,18 @@ class MathsQuiz(object):
     def start_quiz(self):
         questionNumber = 0
         while True:
+
+            # noinspection PyBroadException
             try:
                 maxQuestions = int(input(
                     "Enter the number of questions in integer form. "
-                    )
-                                   )
+                )
+                )
                 break
             except:
                 print(
                     "That is an invalid input. "
-                    )
+                )
                 continue
         for questionNumber in range(1, maxQuestions + 1):
             self.ask_question()
@@ -34,14 +35,14 @@ class MathsQuiz(object):
                 .format(self.studentScore,
                         questionNumber
                         )
-                )
+            )
         print(
             "{} , your final score was {} / {}"
             .format(self.studentName,
                     self.studentScore,
                     questionNumber
                     )
-            )
+        )
 
     def random_generator(self):
         self.randomNumber1 = randint(0, 10)
@@ -56,6 +57,7 @@ class MathsQuiz(object):
                     "-": sub,
                     "*": mul}
         while True:
+            # noinspection PyBroadException
             try:
                 studentGuess = int(input(
                     "Enter the answer to {} {} {}\n"
@@ -63,25 +65,26 @@ class MathsQuiz(object):
                             self.randomSign,
                             self.randomNumber2
                             )
-                    )
-                                   )
+                )
+                )
                 break
             except:
                 print(
                     "That is an invalid answer, try again."
-                    )
+                )
                 continue
+        # noinspection PyTypeChecker
         if studentGuess == operator[self.randomSign](self.randomNumber1,
                                                      self.randomNumber2
                                                      ):
             print(
                 "Correct."
-                )
+            )
             self.studentScore += 1
         else:
             print(
                 "Incorrect."
-                )
+            )
 
 
 def startup():
@@ -89,22 +92,22 @@ def startup():
         "This is a basic Maths quiz,"
         "answer each question,"
         "at the end you will get a score.\n\n"
-        )
+    )
     while True:
         quizOrQuit = input(
             "Do you wish to participate? (Y/N)"
-            ).lower()
-        if quizOrQuit not in ("yesno"):
+        ).lower()
+        if quizOrQuit not in "yesno":
             print(
                 "Not a valid input."
-                )
+            )
         else:
             break
     if quizOrQuit in "yes":
         studentName = str(input(
             "What is your name? "
-            )
-                          )
+        )
+        )
         student_users[studentName] = MathsQuiz(studentName)
         student_users[studentName].start_quiz()
     else:
