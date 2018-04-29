56428a5e474abb75d922f0d23ea7d8984d27589f
SamDerp <samuelderpyson@gmail.com>
2018-04-29 02:14:02 +0100

    Added a numpad to the MathsQuiz
    Added numpad, rejoined the questions into a single function, made the numpad work nice now, and a couple other minor changes.

--- Classes/MathsQuiz.py
+++ Classes/MathsQuiz.py
@@ -1,7 +1,8 @@
 from random import randint, choice
 from operator import add, mul, sub
 from uuid import uuid4
-
+from tkinter import *
+from tkinter import ttk
 
 class MathsQuiz(object):
     def __init__(self):
@@ -11,10 +12,12 @@ class MathsQuiz(object):
         self.randomSign = 0
         self.studentScore = 0
         self.uniqueID = 0
+        self.questionNumber = 0
+        self.maxQuestion = 0
         self.maxAddition = 0
         self.maxSubtraction = 0
         self.maxMultiplication = 0
-
+        self.startup()
     def startup(self):
         self.studentName = str(input(
             "What is your name? "
@@ -47,12 +50,11 @@ class MathsQuiz(object):
         self.start_quiz()
 
     def start_quiz(self):
-        questionNumber = 0
         while True:
 
             # noinspection PyBroadException
             try:
-                maxQuestions = int(input(
+                self.maxQuestions = int(input(
                     "Enter the number of questions in integer form. "
                 )
                 )
@@ -104,51 +106,51 @@ class MathsQuiz(object):
                     "That is an invalid input. "
                 )
                 continue
-        for questionNumber in range(1, maxQuestions + 1):
-            if self.maxMultiplication >= self.studentScore >= self.maxSubtraction:
-                self.ask_multiplication()
-            elif self.maxSubtraction >= self.studentScore >= self.maxAddition:
-                self.ask_subtraction()
-            else:
-                if self.studentScore <= self.maxAddition:
-                    self.ask_addition()
+        self.generate_calculator()
+        self.question_checker()
+        
+    def question_checker(self):
+        if self.questionNumber == self.maxQuestions:
             print(
-                "\nYou are currently on {} points."
-                .format(self.studentScore,
+                "{} , your final score was {}"
+                .format(self.studentName,
+                        self.studentScore
                         )
             )
-        print(
-            "{} , your final score was {} / {}"
-            .format(self.studentName,
-                    self.studentScore,
-                    questionNumber
-                    )
-        )
+
+        else:
+            self.symbol_checker()
+            self.ask_question()
+            self.questionNumber = self.questionNumber + 1
+            print("\nYou are currently on {} points.".format(self.studentScore))
+
+    def symbol_checker(self):
+        if self.maxMultiplication >= self.studentScore >= self.maxSubtraction:
+            self.symbol = "*"
+        elif self.maxSubtraction >= self.studentScore >= self.maxAddition:
+            self.symbol = "-"
+        else:
+            if self.studentScore <= self.maxAddition:
+                self.symbol = "+"
+
+    def ask_question(self):
+        self.random_generator()
+        self.symbol_checker()
+        self.studentGuess = IntVar()
+        self.addQuestion = StringVar()
+        self.operator = {"+": add,
+                         "-": sub,
+                         "*": mul}
+        self.question = "Enter the answer to {} {} {}\n".format(self.randomNumber1, self.symbol, self.randomNumber2)
+        self.mathsQuestion.set(self.question)
 
     def random_generator(self):
         self.randomNumber1 = randint(0, 10)
         self.randomNumber2 = randint(0, 10)
 
-    def ask_addition(self):
-        self.random_generator()
-        while True:
-            # noinspection PyBroadException
-            try:
-                studentGuess = int(input(
-                    "Enter the answer to {} + {}\n"
-                    .format(self.randomNumber1,
-                            self.randomNumber2
-                            )
-                )
-                )
-                break
-            except:
-                print(
-                    "That is an invalid answer, try again."
-                )
-                continue
-        # noinspection PyTypeChecker
-        if studentGuess == add(self.randomNumber1,self.randomNumber2):
+    def check_math(self):
+        print(self.studentInput.get())
+        if self.studentInput.get() == self.operator[self.symbol](self.randomNumber1, self.randomNumber2):
             print(
                 "Correct."
             )
@@ -158,68 +160,56 @@ class MathsQuiz(object):
                 "Incorrect."
             )
             self.studentScore -= 1
+        self.studentInput.set(0)
+        self.question_checker()
 
-    def ask_subtraction(self):
-        self.random_generator()
-        while True:
-            # noinspection PyBroadException
-            try:
-                studentGuess = int(input(
-                    "Enter the answer to {} - {}\n"
-                    .format(self.randomNumber1,
-                            self.randomNumber2
-                            )
-                )
-                )
-                break
-            except:
-                print(
-                    "That is an invalid answer, try again."
-                )
-                continue
-        # noinspection PyTypeChecker
-        if studentGuess == sub(self.randomNumber1,self.randomNumber2):
-            print(
-                "Correct."
-            )
-            self.studentScore += 1
-        else:
-            print(
-                "Incorrect."
-            )
-            self.studentScore -= 1
+    def generate_calculator(self):
+        root = Tk() 
+        root.title("Maths Quiz")
+        self.mathsQuestion = StringVar()
+        self.studentInput = IntVar()
+        correct = StringVar()
+        self.ask_question()
+        mathsQuestionLabel = Label(root, textvariable=self.mathsQuestion, height=2, width=20)
+        mathsQuestionLabel.grid(row=1, column=0, sticky=N)
+        studentInputLabel = Label(root, textvariable=self.studentInput, height=2, width=20)
+        studentInputLabel.grid(row=2, column=0, sticky=N)
+        Grid.rowconfigure(root, 0, weight=1) 
+        Grid.columnconfigure(root, 0, weight=1)
+        frame=Frame(root)
+        frame.grid(row=0, column=0, sticky=N+S+E+W)
+        rowIndex = 0 
+        buttonText = 1 
+        while rowIndex != 4:
+            Grid.rowconfigure(frame, rowIndex, weight=1) 
+            rowIndex = rowIndex + 1
+            colIndex = 0 
+            
+            while colIndex != 3: 
+                Grid.columnconfigure(frame, colIndex, weight=1) 
+                btn = ttk.Button(frame) 
+                btn.grid(row=rowIndex, column=colIndex, sticky=N+S+E+W) 
+                colIndex = colIndex + 1 
+                btn['text'] =buttonText
+                btn['command'] =lambda x=buttonText: self.studentInput.set(self.studentInput.get()*10 + x)
+                buttonText = buttonText + 1
 
-    def ask_multiplication(self):
-        self.random_generator()
-        while True:
-            # noinspection PyBroadException
-            try:
-                studentGuess = int(input(
-                    "Enter the answer to {} * {}\n"
-                    .format(self.randomNumber1,
-                            self.randomNumber2
-                            )
-                )
-                )
-                break
-            except:
-                print(
-                    "That is an invalid answer, try again."
-                )
-                continue
-        # noinspection PyTypeChecker
-        if studentGuess == mul(self.randomNumber1,self.randomNumber2):
-            print(
-                "Correct."
-            )
-            self.studentScore += 1
-        else:
-            print(
-                "Incorrect."
-            )
-            self.studentScore -= 1
+                if buttonText ==11: 
+                    btn['text'] ="-"
+                    btn['command'] =lambda : self.studentInput.set(-self.studentInput.get())
 
+                if buttonText ==12: 
+                    btn['text'] ="⌫"
+                    btn['command'] =lambda : self.studentInput.set(0)
 
-    
+                if buttonText == 13:
+                    btn['text'] ="↵"
+                    btn['command'] =lambda : self.check_math()
+
+                btn = ttk.Button(frame) 
+                btn.grid(row=5, column=1, sticky=N+S+E+W) 
+                btn['text'] ="0"
+                btn['command'] =lambda : self.studentInput.set(self.studentInput.get()*10)
+        root.mainloop()
 
 
